0b4413e3e8a962762ab5bbae00e404e1145a4694
Abhi G <abhijeet.goel@testaing.com>
2020-10-11 04:52:54 +0530

    updates 0.1

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express()
+const port = 3000
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.get('/', (req,res)=>{
+	res.send('hello');
+});
+
+app.get('/balance', (req,res)=>{
+	var balance = {balance: 0};
+	res.send(balance);
+});
+
+
+app.get('/status', (req,res)=>{
+	var status = {status: 0};
+	res.send(status);
+});
+
+app.get('/account_providers', (req,res)=>{
+	var providers = [{'bank1':'neft'},{'bank2':'neft'}];
+	res.send(providers);
+});
+
+app.post('/pay', (req,res)=>{
+	const query = req.query;
+	console.log(req.query.name + " " + req.query.phone + " " + req.query.secret_key);
+	res.send({status: 0});
+});
+
+app.listen(port, ()=>{
+	console.log(`Server running at http://localhost:${port}`);
+});
